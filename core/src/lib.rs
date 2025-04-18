4e2089cf15784be897f08e06dde2391b5613d893
shoaky <shoaky159@gmail.com>
2025-04-18 18:43:02 +0800

    feat: 从文件读取component定义(临时提交)

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,8 +3,9 @@ mod processor_manager;
 mod container;
 mod components;
 mod core;
+mod config;
+mod instance_manager;
 
 pub use component_manager::*;
 pub use container::*;
 pub use core::*;
-// pub use processor_manager::*;
