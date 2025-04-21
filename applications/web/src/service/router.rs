b7f3ff3c97faf642d51810ff5beb34bae265f16e
fcw <fcwdwyyx@163.com>
2025-04-22 00:33:49 +0800

    feat: 抽象dao层 并实现基于yaml file的trait

--- applications/web/src/service/router.rs
+++ applications/web/src/service/router.rs
@@ -1,17 +1,19 @@
+use crate::dao::yaml_file::YamlFileDao;
+use crate::dao::ComponentDao;
+use crate::error::error_handle::{error_handler, AppResult};
+use crate::model::http_model::ApiResponse;
 use axum::extract::State;
-use axum::{Router, middleware, routing::get};
+use axum::{middleware, routing::get, Router};
 use core::CoreApplication;
 use std::sync::{Arc, RwLock};
 
-use crate::error::error_handle::{AppError, AppResult, error_handler};
-use crate::model::http_model::ApiResponse;
-
-pub async fn register_routers(core_application: Arc<RwLock<CoreApplication>>) -> Router {
+pub fn register_routers(core_application: Arc<RwLock<CoreApplication>>) -> Router {
+    let dao: Arc<dyn ComponentDao> = Arc::new(YamlFileDao::new(core_application.clone()));
     Router::new()
         .route("/", get(handler))
         .route("/suppliers", get(list_component_suppliers))
         .layer(middleware::from_fn(error_handler))
-        .with_state(core_application)
+        .with_state(dao)
 }
 
 async fn handler() -> ApiResponse<String> {
@@ -20,35 +22,11 @@ async fn handler() -> ApiResponse<String> {
 
 #[axum::debug_handler]
 async fn list_component_suppliers(
-    State(core_application): State<Arc<RwLock<CoreApplication>>>,
+    State(dao): State<Arc<dyn ComponentDao>>,
 ) -> AppResult<Vec<String>> {
-    // 使用 ? 运算符处理错误，会自动转换为 AppError
-    let app = core_application
-        .read()
-        .map_err(|e| AppError::InternalError(format!("Failed to acquire read lock: {}", e)))?;
-
-    // 访问 component_manager
-    let component_manager = app.component_manager.read().map_err(|e| {
-        AppError::InternalError(format!("Failed to access component manager: {}", e))
-    })?;
-
-    // 使用 ? 运算符优雅处理错误
-    let suppliers = component_manager
-        .get_all_suppliers()
-        .map_err(|e| AppError::InternalError(format!("Failed to get suppliers: {}", e)))?;
-
-    // 处理数据
-    let supplier_types: Vec<String> = suppliers
-        .iter()
-        .flat_map(|supplier| {
-            supplier
-                .supply_types()
-                .iter()
-                .map(|c_type| c_type.name.clone())
-                .collect::<Vec<String>>()
-        })
-        .collect();
-
+    // 调用 dao 的方法获取组件供应商列表
+    let supplier_types = dao.list_component_suppliers()?;
     // 成功返回
     Ok(ApiResponse::success(supplier_types))
 }
+
